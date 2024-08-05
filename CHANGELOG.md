4c416c4eab9c2027106e0e8668bec0f2383e817b
oyajiDev <this.dev.somehit@gmail.com>
2024-08-05 18:17:51 +0900

    fix rollup file

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+
+## 2023/10/13
+- initial release.
+
+## 2024/08/05
+- update dependencies.
+- add `@polight/store`.
+- fix `rollup.config.js` file(for lego-store).
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,23 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
 export default {
     input: "build/bricks/index.js",
     output: {
         file: "dist/bricks/index.js",
-        format: "cjs"
-    }
+        format: "iife"
+    },
+    plugins: [
+        commonjs({
+            include: [
+                "node_modules/**"
+            ],
+            ignoreGlobal: false,
+            sourceMap: false
+        }),
+        nodeResolve({
+            jsnext: true,
+            main: false
+        })
+    ]
 };

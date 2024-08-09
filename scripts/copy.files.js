67924dc7efdd63a590fe3246d5f1149ee503ec28
oyajiDev <this.dev.somehit@gmail.com>
2024-08-09 20:08:50 +0900

    move copy.files.js to scripts directory

--- scripts/copy.files.js
+++ scripts/copy.files.js
@@ -4,15 +4,15 @@ import * as path from "path";
 import { fileURLToPath } from "url";
 import { globSync } from "glob";
 import * as fs from "fs";
-import { default as legoConfig } from "./lego.config.js";
-import { default as rollupConfig } from "./rollup.config.js";
+import { default as legoConfig } from "../lego.config.js";
+import { default as rollupConfig } from "../rollup.config.js";
 
 
 const parser = ArgumentParser();
 parser.add_argument("copyType", { type: "str" });
 const args = parser.parse_args();
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
 const sourceDir = path.join(__dirname, path.dirname(legoConfig.sourceDir));
 
 
@@ -29,9 +29,33 @@ if (args.copyType == "build") {
         fs.mkdirSync(destDir, { recursive: true });
     }
 
+    const storeDir = path.join(__dirname, "node_modules", "@polight", "store");
+    var storeScript = null;
+    if (fs.existsSync(storeDir)) {
+        storeScript = fs.readFileSync(path.join(storeDir, "dist", "store.js"), { encoding: "utf-8" });
+    }
+
     for (var srcFile of globSync(path.join(sourceDir, "*.*"), { ignore: "**/index.rollup.html" })) {
         const srcFileName = path.basename(srcFile);
-        fs.copyFileSync(srcFile, path.join(destDir, srcFileName));
+        const destFile = path.join(destDir, srcFileName);
+        fs.copyFileSync(srcFile, destFile);
+
+        if (storeScript != null) {
+            try {
+                const fileContent = fs.readFileSync(destFile, { encoding: "utf-8" });
+                if (fileContent.includes(`import { LegoStore } from "@polight/store";`)) {
+                    fs.writeFileSync(
+                        destFile,
+                        fileContent.replace(
+                            `import { LegoStore } from "@polight/store";`,
+                            storeScript.replace(`export { AbstractStore, LegoStore };`, "")
+                        ),
+                        { encoding: "utf-8" }
+                    );
+                }
+            }
+            catch {}
+        }
     }
 }
 else if (args.copyType == "rollup") {
